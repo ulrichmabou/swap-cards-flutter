31a599b0199505836ce7bb53a1195c569726073b
UlrichMabou <ulrichmabou@gmail.com>
2020-02-11 20:22:44 +0300

    add simple example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,31 +9,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Swappies',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Swappies'),
+      theme: ThemeData.dark(),
+      home: ExampleHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({this.title});
-
-  final String title;
-
+class ExampleHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ExampleHomePageState createState() => _ExampleHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _ExampleHomePageState extends State<ExampleHomePage>
+    with TickerProviderStateMixin {
+  List<String> welcomeImages = [
+    "assets/welcome0.png",
+    "assets/welcome1.png",
+    "assets/welcome2.png",
+    "assets/welcome2.png",
+    "assets/welcome1.png",
+    "assets/welcome1.png"
+  ];
+
   @override
   Widget build(BuildContext context) {
+    //Use this to trigger swap.
+    CardController controller;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Swap Cards'),
       ),
-      body: Container(),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: TinderSwapCard(
+              orientation: AmassOrientation.BOTTOM,
+              totalNum: 6,
+              stackNum: 3,
+              swipeEdge: 4.0,
+              maxWidth: MediaQuery.of(context).size.width * 0.9,
+              maxHeight: MediaQuery.of(context).size.width * 0.9,
+              minWidth: MediaQuery.of(context).size.width * 0.8,
+              minHeight: MediaQuery.of(context).size.width * 0.8,
+              cardBuilder: (context, index) => Card(
+                    child: Image.asset('${welcomeImages[index]}'),
+                  ),
+              cardController: controller = CardController(),
+              swipeUpdateCallback:
+                  (DragUpdateDetails details, Alignment align) {
+                /// Get swiping card's alignment
+                if (align.x < 0) {
+                  //Card is LEFT swiping
+                } else if (align.x > 0) {
+                  //Card is RIGHT swiping
+                }
+              },
+              swipeCompleteCallback:
+                  (CardSwipeOrientation orientation, int index) {
+                /// Get orientation & index of swiped card!
+              }),
+        ),
+      ),
     );
   }
 }
